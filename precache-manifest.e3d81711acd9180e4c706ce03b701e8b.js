88f5db82e68612b3467471954930c8275de6f0bb
Abdul Malik <abdul007malik@hotmail.com>
2020-07-27 00:09:50 +0530

    Updates

---
+++ precache-manifest.e3d81711acd9180e4c706ce03b701e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d30adbae00923316ec8c557fcba575a",
+    "url": "/quiz-assignment/index.html"
+  },
+  {
+    "revision": "3183e58c25282b4a78aa",
+    "url": "/quiz-assignment/static/css/main.cc8d4e0e.chunk.css"
+  },
+  {
+    "revision": "d1a9f3575fece00b8bff",
+    "url": "/quiz-assignment/static/js/2.8b0dea76.chunk.js"
+  },
+  {
+    "revision": "6a0b7199aeaa1d579235ac6667857c30",
+    "url": "/quiz-assignment/static/js/2.8b0dea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3183e58c25282b4a78aa",
+    "url": "/quiz-assignment/static/js/main.c7b7bf6e.chunk.js"
+  },
+  {
+    "revision": "8ac97edcb1462aa2d786",
+    "url": "/quiz-assignment/static/js/runtime-main.2f3bc22a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a361c9ec87cd5d507895942ea103849f.js"
+  "/quiz-assignment/precache-manifest.e3d81711acd9180e4c706ce03b701e8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz-assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
